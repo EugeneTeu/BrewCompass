bc9a41f2e84f528cbdd900d675570acdbf6454de
Eugene Teu Chu Wei <eugeneteu@gmail.com>
2019-06-10 23:04:40 +0800

    changed keyboard resizing on convertor

--- coffee_app/lib/utilities/converter.dart
+++ coffee_app/lib/utilities/converter.dart
@@ -25,112 +25,117 @@ class _MyConverter extends State<MyConverter> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            ListTile(
-              title: TextFormField(
-                controller: _controllerTwo,
-                focusNode: _focusNodeTwo,
-                style: Styles.calcFont,
-                decoration: new InputDecoration(
-                  hintText: "Enter Weight Of Beans",
-                  hintStyle: new TextStyle(fontSize: 15.0),
-                ),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                validator: (value) => value.isEmpty ? "invalid number" : null,
-                onSaved: (value) => _inputBeanWeight = value ,
-              ),
-              trailing: MaterialButton(
-                child: Icon(Icons.arrow_downward),
-                onPressed: () {
-                  _focusNodeTwo.unfocus();
-                },
-              ),
-            ),
-            ListTile(
-              title: TextFormField(
-                controller: _controller,
-                focusNode: _focusNode,
-                style: Styles.calcFont,
-                decoration: new InputDecoration(
-                    hintText: "Enter Desired Water Ratio (1:_)",
-                    hintStyle: new TextStyle(fontSize: 15.0)),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                validator: (value) => value.isEmpty ? "invalid number" : null,
-                onSaved: (value) => _inputRatio = value,
-              ),
-              trailing: MaterialButton(
-                child: Icon(
-                  Icons.arrow_downward,
-                ),
-                onPressed: () {
-                  _focusNode.unfocus();
-                },
-              ),
-            ),
-            // Calculate and Clear button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                MaterialButton(
-                  elevation: 10.0,
-                  color: Colors.red,
-                  child: Text("Clear", style: Styles.calcFont,),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
-                      onPressed: (){
-                        //clears input field and reset results field.
-                        setState(() {
-                          _controller.clear();
-                          _controllerTwo.clear();
-                          _ratio = 0.0;
-                          _beanWeight = 0.0;
-                          _water = 0.0;
-                          _inputBeanWeight ='';
-                          _inputRatio ='';
-                        });
-                      },
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        body: Container(
 
-                ),
-                                MaterialButton(
-                  child: Text(
-                    "Calculate",
-                    style: Styles.calcFont,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ListTile(
+                title: TextFormField(
+                  
+                  controller: _controllerTwo,
+                  focusNode: _focusNodeTwo,
+                  style: Styles.calcFont,
+                  decoration: new InputDecoration(
+                    hintText: "Enter Weight Of Beans",
+                    hintStyle: new TextStyle(fontSize: 15.0),
                   ),
-                  color: Colors.grey,
-                  elevation: 10.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  validator: (value) => value.isEmpty ? "invalid number" : null,
+                  onSaved: (value) => _inputBeanWeight = value ,
+                ),
+                trailing: MaterialButton(
+                  child: Icon(Icons.arrow_downward),
                   onPressed: () {
-                    final form = _formKey.currentState;
-                    form.save();
-
-                    setState(() {
-                      _ratio = double.parse(_inputRatio);
-                      _water = double.parse(_inputBeanWeight) * double.parse(_inputRatio);
-                      _beanWeight = double.parse(_inputBeanWeight);
-                    });
+                    _focusNodeTwo.unfocus();
                   },
                 ),
-              ],
-            ),
-            ListTile(
-              title: Text("Ratio", style: Styles.calcFont),
-              trailing: Text("$_ratio"),
-            ),
-            ListTile(
-              title: Text("Water Weight", style: Styles.calcFont),
-              trailing: Text("$_water"),
-            ),
-            ListTile(
-              title: Text("Bean Weight", style: Styles.calcFont),
-              trailing: Text("$_beanWeight"),
-            ),
-          ],
+              ),
+              ListTile(
+                title: TextFormField(
+                  controller: _controller,
+                  focusNode: _focusNode,
+                  style: Styles.calcFont,
+                  decoration: new InputDecoration(
+                      hintText: "Enter Desired Water Ratio (1:_)",
+                      hintStyle: new TextStyle(fontSize: 15.0)),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  validator: (value) => value.isEmpty ? "invalid number" : null,
+                  onSaved: (value) => _inputRatio = value,
+                ),
+                trailing: MaterialButton(
+                  child: Icon(
+                    Icons.arrow_downward,
+                  ),
+                  onPressed: () {
+                    _focusNode.unfocus();
+                  },
+                ),
+              ),
+              // Calculate and Clear button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  MaterialButton(
+                    elevation: 10.0,
+                    color: Colors.red,
+                    child: Text("Clear", style: Styles.calcFont,),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0)),
+                        onPressed: (){
+                          //clears input field and reset results field.
+                          setState(() {
+                            _controller.clear();
+                            _controllerTwo.clear();
+                            _ratio = 0.0;
+                            _beanWeight = 0.0;
+                            _water = 0.0;
+                            _inputBeanWeight ='';
+                            _inputRatio ='';
+                          });
+                        },
+
+                  ),
+                                  MaterialButton(
+                    child: Text(
+                      "Calculate",
+                      style: Styles.calcFont,
+                    ),
+                    color: Colors.grey,
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0)),
+                    onPressed: () {
+                      final form = _formKey.currentState;
+                      form.save();
+
+                      setState(() {
+                        _ratio = double.parse(_inputRatio);
+                        _water = double.parse(_inputBeanWeight) * double.parse(_inputRatio);
+                        _beanWeight = double.parse(_inputBeanWeight);
+                      });
+                    },
+                  ),
+                ],
+              ),
+              ListTile(
+                title: Text("Ratio", style: Styles.calcFont),
+                trailing: Text("$_ratio"),
+              ),
+              ListTile(
+                title: Text("Water Weight", style: Styles.calcFont),
+                trailing: Text("$_water"),
+              ),
+              ListTile(
+                title: Text("Bean Weight", style: Styles.calcFont),
+                trailing: Text("$_beanWeight"),
+              ),
+            ],
+          ),
         ),
       ),
     );
