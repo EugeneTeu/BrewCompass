faf6340ef56e54e4084a53011eb7b18363e5c0f0
Eugene Teu Chu Wei <eugeneteu@gmail.com>
2019-06-25 00:41:09 +0800

    made search bar responsive on focus, automatically hides icon

--- coffee_app/lib/misc/search_bar.dart
+++ coffee_app/lib/misc/search_bar.dart
@@ -2,49 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../styles.dart';
 
-class SearchBar extends StatelessWidget {
-  const SearchBar({
+class SearchBar extends StatefulWidget {
+  SearchBar({
     @required this.controller,
     @required this.focusNode,
   });
 
-  final TextEditingController controller;
-  final FocusNode focusNode;
+  TextEditingController controller;
+  FocusNode focusNode;
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  void initState() {
+    super.initState();
+    widget.focusNode = new FocusNode();
+    widget.focusNode.addListener(_onOnFocusNodeEvent);
+  }
+
+  _onOnFocusNodeEvent() {
+    setState(() {
+      // Re-renders
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: BoxDecoration(
-        color: Styles.searchBackground,
-        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        //borderRadius: BorderRadius.circular(10),
       ),
       child: Padding(
           padding: const EdgeInsets.symmetric(
-            horizontal: 4,
+            horizontal: 10,
             vertical: 8,
           ),
           child: Row(
             children: [
-              const Icon(
-                Icons.search,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: getIcon(),
               ),
               Expanded(
                 child: TextField(
-                  controller: controller,
-                  focusNode: focusNode,
+                  controller: widget.controller,
+                  focusNode: widget.focusNode,
                   style: Styles.searchText,
                   cursorColor: Styles.searchCursorColor,
                 ),
               ),
               GestureDetector(
-                onTap: controller.clear,
-                child: const Icon(
-                  Icons.backspace,
-                  color: Styles.searchIconColor,
+                onTap: widget.controller.clear,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: getCancelIcon(),
                 ),
               ),
             ],
           )),
     );
+  }
+
+  Icon getCancelIcon() {
+    if (widget.focusNode.hasFocus) {
+      return const Icon(
+        Icons.cancel,
+        color: Styles.searchIconColor,
+      );
+    } else {
+      return Icon(
+        Icons.cancel,
+        color: Colors.white70,
+      );
+    }
+  }
+
+  Icon getIcon() {
+    if (widget.focusNode.hasFocus) {
+      return Icon(Icons.search, color: Colors.grey,);
+    } else {
+      return const Icon(
+        Icons.search,
+      );
+    }
   }
 }
